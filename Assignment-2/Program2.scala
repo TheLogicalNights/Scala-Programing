a53c8ccd70e4a535c0740bed83b48d7972959d06
Swapnil Ramesh Adhav <69031236+TheLogicalNights@users.noreply.github.com>
2021-02-07 18:34:05 +0530

    Add files via upload

---
+++ Assignment-2/Program2.scala
@@ -0,0 +1,12 @@
+// Problem Statement : Program to print "Scala" 5 times on the screen
+
+object PrintScala
+{
+    def main(args:Array[String]) : Unit = 
+    {
+      for(i<- 1 to 5)
+      {
+        println("Scala");
+      }
+    }
+}
