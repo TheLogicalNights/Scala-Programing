9e831c0aaae8309113fc4155befc6c46d00a70de
Swapnil Ramesh Adhav <69031236+TheLogicalNights@users.noreply.github.com>
2021-02-13 21:47:30 +0530

    Add files via upload

---
+++ Assignment-4/Program2.scala
@@ -0,0 +1,22 @@
+// Problem Statement :  Write a program which accept number from user and print even factors of that number. 
+
+object Test
+{
+    def main(args:Array[String]) : Unit =
+    {
+        var iNo = 0;
+        println("Enter a number");
+        iNo = scala.io.StdIn.readInt();
+        for(i<- 1 to iNo/2)
+        {
+            if(iNo%i==0)
+            {
+                if(i%2==0)
+                {
+                    print(s"$i\t");
+                }
+            }
+        }
+        println();
+    }
+}
