374a6b4958dd6452e7492fe493361a22dda145b9
Swapnil Ramesh Adhav <69031236+TheLogicalNights@users.noreply.github.com>
2021-02-14 13:58:28 +0530

    Add files via upload

---
+++ Assignment-6/Program4.scala
@@ -0,0 +1,20 @@
+// Problem Statement : Write a program which accept number from user and return summation of all its non factors.
+
+object Test
+{
+    def main(args:Array[String]) : Unit = 
+    {
+        var iNo = 0;
+        var iSum = 0;
+        println("Enter a number");
+        iNo = scala.io.StdIn.readInt();
+        for(i<- 1 to iNo-1)
+        {
+            if(iNo%i!=0)
+            {
+                iSum = iSum+i;
+            }
+        }
+        println(s"Sum of non-factors is : $iSum");
+    }
+}
